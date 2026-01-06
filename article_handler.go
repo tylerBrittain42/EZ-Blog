1ab7ae2dd25db37c6e0f8d247ff3e7742267f07c
Tyler Brittain <tbrittain100@gmail.com>
2026-01-05 22:17:54 -0800

    implemenet general and specific article handlers

---
+++ article_handler.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"bytes"
+	"html/template"
+	"log"
+	"net/http"
+
+	"github.com/tylerBrittain42/blog/pkg/basicArticle"
+	"github.com/tylerBrittain42/blog/pkg/validator"
+)
+
+func (cfg *config) generalArticleHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "template/toc.html")
+}
+
+func (cfg *config) specificArticleHandler(w http.ResponseWriter, r *http.Request) {
+	cfg.name = r.PathValue("name")
+	isSanitized, err := validator.IsAlphaNumeric(cfg.name)
+	if err != nil {
+		respondWithError(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if !isSanitized {
+		respondWithError(w, "Invalid characters in name", http.StatusNotAcceptable)
+		return
+	}
+
+	bArt := basicArticle.BasicArticle{}
+	fullPath, err := bArt.GetFilePath(cfg.templateDir, cfg.name)
+	if err != nil {
+		respondWithError(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	canAccess, err := validator.IsAccessible(fullPath)
+	if err != nil {
+		respondWithError(w, err.Error(), http.StatusInternalServerError)
+
+		return
+	}
+	if !canAccess {
+		respondWithError(w, "Unable to access article", http.StatusNotAcceptable)
+		return
+	}
+	log.Println("able to access")
+
+	// doing template stuff here
+	// remove line above and also this comment
+	// use name from above
+
+	// CONSIDER: how to handle spaces? swap name with underscore, but we already do not allow symbols so maybe table this as future issue
+	// could also be diff in the metadata version so don't trip
+
+	articleBytes, _ := cfg.getTemplate(bArt)
+	_, err = w.Write(articleBytes)
+	if err != nil {
+		log.Printf("Unable to write articleBytes, %v\n", err)
+	}
+}
+
+func (cfg *config) getTemplate(a articleCreator) ([]byte, error) {
+	type article struct {
+		Title   string
+		Content string
+	}
+	fullName, err := a.GetFilePath(cfg.templateDir, cfg.name)
+	if err != nil {
+		return nil, err
+	}
+
+	title, err := a.GetTitle(fullName)
+	if err != nil {
+		return nil, err
+	}
+
+	content, err := a.GetContent(fullName)
+	if err != nil {
+		return nil, err
+	}
+
+	t, err := template.ParseFiles("template/base.html")
+	if err != nil {
+		return nil, err
+	}
+
+	var buf bytes.Buffer
+	data := article{
+		Title:   title,
+		Content: content,
+	}
+	if err := t.Execute(&buf, data); err != nil {
+		return nil, err
+	}
+
+	return buf.Bytes(), nil
+}
--- main.go
+++ main.go
@@ -1,16 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/joho/godotenv"
-	"github.com/tylerBrittain42/blog/pkg/validator"
 )
 
-type article interface {
+type articleCreator interface {
 	GetFilePath(dir string, name string) (string, error)
 	GetTitle(fileName string) (string, error)
 	GetContent(fileName string) (string, error)
@@ -31,7 +29,8 @@ func main() {
 	cfg := config{templateDir: os.Getenv("DIR")}
 
 	mux.HandleFunc("/", indexHandler)
-	mux.HandleFunc("GET /article/{name}", cfg.articleHandler)
+	mux.HandleFunc("GET /article/", cfg.generalArticleHandler)
+	mux.HandleFunc("GET /article/{name}", cfg.specificArticleHandler)
 
 	log.Printf("Serving on port %s\n", port)
 	log.Fatal(server.ListenAndServe())
@@ -42,56 +41,7 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "template/index.html")
 }
 
-func (cfg *config) articleHandler(w http.ResponseWriter, r *http.Request) {
-	name := r.PathValue("name")
-	isSanitized, err := validator.IsAlphaNumeric(name)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(fmt.Sprintf("error: %v\n", err)))
-		return
-	}
-	if isSanitized == false {
-		w.WriteHeader(http.StatusNotAcceptable)
-		w.Write([]byte("Invalid characters in name"))
-		return
-	}
-
-	nameExt := name + ".md"
-	canAccess, err := validator.IsAccessible(cfg.templateDir, nameExt)
-	// TODO: improve me
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(fmt.Sprintf("error: %v\n", err)))
-		return
-	}
-	if !canAccess {
-		w.WriteHeader(http.StatusNotAcceptable)
-		w.Write([]byte("Unable to access article"))
-		return
-	}
-
-	http.ServeFile(w, r, "template/index.html")
-
-	// doing template stuff here
-	// remove line above and also this comment
-	// use name from above
-
-	// CONSIDER: how to handle spaces? swap name with underscore, but we already do not allow symbols so maybe table this as future issue
-	// could also be diff in the metadata version so don't trip
-	type article interface {
-		GetTitle(fileName string) string
-		GetContent(fileName string) string
-	}
-
-	// procedure
-	// 1. get title - DONE
-	// 2. get content
-	// 2.1 if empty, then return a custom error message
-	// 3. render template
-	// 4. return template
-
-}
-
 type config struct {
 	templateDir string
+	name        string
 }
